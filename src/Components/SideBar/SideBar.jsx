9ce3c1b59f4b680c273c99d6f87bf7c30a12d079
itsmechaithanya <itsmechaithanyam@gmail.com>
2024-08-10 16:20:38 +0530

    first commit

---
+++ src/Components/SideBar/SideBar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const SideBar = () => {
+  return (
+    <div className='h-screen w-screen bg-slate-600'>
+        <h1 className='bg-slate-500'>hey</h1>
+    </div>
+  )
+}
+
+export default SideBar
