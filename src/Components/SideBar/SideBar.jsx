572a7548115be571ed952363a8de6a7d3b033181
itsmechaithanya <itsmechaithanyam@gmail.com>
2024-08-13 22:24:58 +0530

    first commit

--- src/Components/SideBar/SideBar.jsx
+++ src/Components/SideBar/SideBar.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const SideBar = () => {
   return (
     <div className='h-screen w-screen bg-slate-600'>
-        <h1 className='bg-slate-500'>hey</h1>
+        <h1 className='font-s bg-slate-500'>hey</h1>
     </div>
   )
 }
